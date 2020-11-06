142f86dfa3bf962a815244bc6cc44e1ccb45c9ac
Gautier <gautier.deleglise@ediservices.com>
2020-11-06 08:50:32 +0100

    :sparkles: chat

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
+const io = require('socket.io')(http);
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chatMessage', (msg) => {
+    io.emit('chatMessage', msg);
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
